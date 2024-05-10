2395e63418315efe478a7e4b92405feec1832d28
AlhandraM <110093031+AlhandraM@users.noreply.github.com>
2024-05-10 08:18:02 -0400

    Update BotCard.js

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function BotCard({ bot, clickEvent, deleteBot, releaseBot }) {
+  // Define classes for different bot types
   const botTypeClasses = {
     Assault: "icon military",
     Defender: "icon shield",
@@ -12,37 +13,46 @@ function BotCard({ bot, clickEvent, deleteBot, releaseBot }) {
 
   return (
     <div className="ui column">
+      {/* Bot Card */}
       <div
         className="ui card"
         key={bot.id}
         onClick={() => (releaseBot ? releaseBot() : clickEvent(bot))}
       >
+        {/* Bot Image */}
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
         </div>
+        {/* Bot Content */}
         <div className="content">
+          {/* Bot Name and Type */}
           <div className="header">
             {bot.name}
             <i className={botTypeClasses[bot.bot_class]} />
           </div>
+          {/* Bot Catchphrase */}
           <div className="meta text-wrap">
             <small>{bot.catchphrase}</small>
           </div>
         </div>
+        {/* Bot Stats */}
         <div className="extra content">
+          {/* Bot Health */}
           <span>
             <i className="icon heartbeat" />
             {bot.health}
           </span>
-
+          {/* Bot Damage */}
           <span>
             <i className="icon lightning" />
             {bot.damage}
           </span>
+          {/* Bot Armor */}
           <span>
             <i className="icon shield" />
             {bot.armor}
           </span>
+          {/* Delete Button */}
           <span>
             <div className="ui center aligned segment basic">
               <button
