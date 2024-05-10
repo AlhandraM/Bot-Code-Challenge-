0cdcf8f398e2f320845cc95441e3be56e6351d2d
AlhandraM <110093031+AlhandraM@users.noreply.github.com>
2024-05-10 08:08:45 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React from "react";
-import BotsPage from "./BotsPage";
+import React from "react"; // Importing React library for building UI components
+import BotsPage from "./BotsPage"; // Importing BotsPage component from the './BotsPage' file
 
-function App() {
+function App() { // Define a functional component named App
   return (
-    <div className="App">
-      <BotsPage />
-    </div>
+    <div className="App"> {/* Start of the root container div with class name 'App' */}
+      <BotsPage /> {/* Render the BotsPage component */}
+    </div> {/* End of the root container div */}
   );
 }
 
-export default App;
+export default App; // Export the App component as default
+
